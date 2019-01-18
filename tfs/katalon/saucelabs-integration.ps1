6b6c39131ad755d942a139d8a21e4453139f14bb
Ryan Manns <rycornell@hotmail.com>
2019-01-17 20:58:01 -0500

    Support multiple desired capabilities and creds

--- tfs/katalon/saucelabs-integration.ps1
+++ tfs/katalon/saucelabs-integration.ps1
@@ -1,9 +1,8 @@
-$filename = "$env:BUILD_SOURCESDIRECTORY\settings\internal\com.kms.katalon.core.webui.remote.properties"
-
-$propFile = [IO.File]::ReadAllText($filename) | ConvertFrom-Json
-
-$propFile.REMOTE_WEB_DRIVER | Add-Member -Type NoteProperty -Name "build" -Value $env:SAUCE_BUILD_NAME
-
-$propFile.REMOTE_WEB_DRIVER | Add-Member -Type NoteProperty -Name "name" -Value $env:SAUCE_BUILD_NAME
-
-$propFile | ConvertTo-Json  | Set-Content $filename
+Get-ChildItem "$env:BUILD_SOURCESDIRECTORY\settings" -Filter "com.kms.katalon.core.webui.remote.properties" -Recurse | % {
+  $propFile = [IO.File]::ReadAllText($_.FullName) | ConvertFrom-Json  
+  $propFile.REMOTE_WEB_DRIVER | Add-Member -Type NoteProperty -Name 'build' -Value $env:SAUCE_BUILD_NAME
+  $propFile.REMOTE_WEB_DRIVER | Add-Member -Type NoteProperty -Name 'name' -Value $env:SAUCE_BUILD_NAME
+  $propFile.REMOTE_WEB_DRIVER.remoteWebDriverUrl = $propFile.REMOTE_WEB_DRIVER.remoteWebDriverUrl.Replace('${SAUCE_USERNAME}', $env:SAUCE_USERNAME)
+  $propFile.REMOTE_WEB_DRIVER.remoteWebDriverUrl = $propFile.REMOTE_WEB_DRIVER.remoteWebDriverUrl.Replace('${SAUCE_ACCESS_KEY}', $env:SAUCE_ACCESS_KEY)
+  $propFile | ConvertTo-Json  | set-content $_.FullName
+}
